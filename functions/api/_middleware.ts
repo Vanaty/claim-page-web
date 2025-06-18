80f287c82d72d6090ab6df6b9d22fe10f710bcde
Termux Kaly <75486658+Vanaty@users.noreply.github.com>
2025-06-18 10:58:58 +0300

    .

--- functions/api/_middleware.ts
+++ functions/api/_middleware.ts
@@ -3,7 +3,9 @@
 export async function onRequest(context, env) {
     const url = new URL(context.request.url)
     const backendUrl = `${env.VITE_API_BASE_URL}${url.pathname}${url.search}`
-  
+    if (!env.VITE_API_BASE_URL) {
+      return new Response('API base URL is not set', { status: 500 })
+    }
     const newRequest = new Request(backendUrl, {
       method: context.request.method,
       headers: context.request.headers,
